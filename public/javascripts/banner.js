5cfa161174147e6afa0e012e27af23a448547c1b
Ram <ram@cloudflare.com>
2014-02-05 12:12:43 -0800

    Change the app module to load widget directly from CloudFront

--- public/javascripts/banner.js
+++ public/javascripts/banner.js
@@ -1,41 +1,10 @@
-CloudFlare.define(
-	'thedaywefightback',
-	['cloudflare/dom', 'thedaywefightback/config', 'cloudflare/jquery1.9'],
-	function(dom){
-		var tdwfb_banner = {};
+CloudFlare.define('thedaywefightback',[
+	'thedaywefightback/config'
+],
+function(
+	config
+){
 
-		tdwfb_banner.showBanner = function(){
-			var banner = dom.createElement('div');
-			//populate div
-			document.body.appendChild(banner);
-
-			return banner;
-		};
-
-		//Check to make sure it's Feb 11th
-		var isTheDay = function(){
-			var date = new Date();
-			var day = date.getDate();
-			var month = date.getMonth()+1;
-			var year = date.getFullYear();
-
-			if( year == '2014' && month == '2' && day == '11' ){
-				return 1;
-			}
-
-			return 0;
-		};
-
-
-		dom.onLoad.then(
-			function(){
-				if( isTheDay() ){
-					var banner = tdwfb_banner.showBanner();
-				}
-			}
-		);
-		
-		return tdwfb_banner;
-
-	}
-);
+	var tdwfb_widget_url = '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js';
+	return CloudFlare.require([tdwfb_widget_url]);
+});
