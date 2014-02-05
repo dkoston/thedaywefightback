cdeaf9a0b6e5ff94bb34e4de66579ea1443e04ce
Ram <ram@cloudflare.com>
2014-02-05 12:41:25 -0800

    Set config values

--- public/javascripts/banner.js
+++ public/javascripts/banner.js
@@ -5,6 +5,8 @@ function(
 	config
 ){
 
+	window.tdwfb_config = config;
+
 	var tdwfb_widget_url = '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js';
 	return CloudFlare.require([tdwfb_widget_url]);
 });
