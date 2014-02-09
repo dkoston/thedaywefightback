a683f217764ec8a9fde164d1576120ecc6d449f8
David Koston <dkoston@cloudflare.com>
2014-02-08 19:21:21 -0800

    cfjs cache removed

--- public/javascripts/banner.js
+++ public/javascripts/banner.js
@@ -11,7 +11,11 @@ function(
 
 	window.tdwfb_config = config;
 
-	var tdwfb_widget_url = '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js';
-	return CloudFlare.require([tdwfb_widget_url]);
+	//var tdwfb_widget_url = '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js';
+	//return CloudFlare.require([tdwfb_widget_url]);
 
+	// use dom.createElement to get around CFJS cache
+        var s = dom.createElement('script');
+        dom.setAttribute(s, 'src', '//d1agz031tafz8n.cloudfront.net/thedaywefightback.js/widget.min.js');
+        document.getElementsByTagName('body')[0].insertBefore(s, null);
 });
